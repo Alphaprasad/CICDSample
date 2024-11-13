3e6973f1d2db00041063ce03a41f33fd5d11ddec
Alphaprasad <prasad.pillay@alpha-ict.in>
2024-11-13 12:58:12 +0530

    Add project files.

---
+++ CICDDemo/Controllers/SampleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CICDDemo.Controllers
+{
+    public class SampleController : Controller
+    {
+        [HttpGet("GetText")]
+        public String GetDemo()
+        {
+            return "Hello";
+        }
+    }
+}
